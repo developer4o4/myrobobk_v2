d85029e7e6d560dd10ccd5a0ae6234cbae43b1c5
izzatillo <adxamovizzatillo008@gmail.com>
2026-05-08 02:55:18 +0500

    fix

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -12,7 +12,7 @@ ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
 CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
-VDOCIPHER_API_SECRET = os.getenv('VDOCIPHER_API_SECRET')
+KINESCOPE_SECRET_KEY = os.getenv('KINESCOPE_SECRET_KEY')
 # ──────────────────────────── APPS ────────────────────────────
 INSTALLED_APPS = [
     "django.contrib.admin",
